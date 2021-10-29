2d3ce00d048379a314db9f729368b1761ff12065
lokeshsloki <lokeshs1432@gmail.com>
2021-10-29 13:55:17 +0530

    files

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function check(){
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+if(q1=="javascript"){c++}
+if(q2=="shimogga"){c++}
+if(q3=="2020"){c++}
+if(q4=="dosa"){c++}
+if(q5=="9"){c++}
+document.write(c);
+}
